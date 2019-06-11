0c55d5b31e8fa4a0612b5f6bce0630f7cd63df7f
Lucas Vieira <lucas.engen@hotmail.com>
2019-06-11 03:21:56 -0400

    Primeira versao do Echo Server
    Signed-off-by: Lucas Vieira <lucas.engen@hotmail.com>

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+    Simples Echo Server TCP
+
+    Autor: Lucas Vieira de Jesus
+*/
+
+using System;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+    class Program
+    {
+        class cmdline_options 
+        {
+            public static string option_ip = "--ip=";
+            public static string option_port = "--port=";
+        }
+
+        public static string ip = "";
+        public static int port = 8085;
+
+        public static bool have_port = false;
+        public static bool have_ip = false;
+
+        public static TcpListener listener = null;
+
+        public static void HandleCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Console.WriteLine( "Parando ...");
+            
+             if(listener != null)
+                listener.Stop();
+        }
+
+        static void Main(string[] args)
+        {
+            Server server;
+            
+            ParseArgs(args);
+
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(HandleCancelKeyPress);
+
+            // Inicializa o servidor
+            server = new Server();
+
+            if(have_port)
+                listener = server.Initialize(ip, port);
+            else
+                listener = server.Initialize(ip);
+
+            while(true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Socket sock = client.Client;
+                byte[] data = new byte[512];
+
+                Console.WriteLine( "Cliente conectado" );
+                sock.Receive(data);
+                sock.Send(data);
+
+                Console.WriteLine( "Fechando conexão...");
+                sock.Close();
+            }
+        }
+
+        static void ParseArgs(string[] args)
+        {
+            foreach(string s in args) 
+            {
+                if(s.StartsWith(cmdline_options.option_ip))
+                {
+                    ip = s.Substring(s.IndexOf("=") + 1);
+                    have_ip = true;
+                } else if(s.StartsWith(cmdline_options.option_port)) {
+                    port = Convert.ToInt16(s.Substring(s.IndexOf("=") + 1));
+                    have_port = true;
+                }
+            }
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,64 @@
+/*
+    Simples Echo Server TCP
+
+    Autor: Lucas Vieira de Jesus
+*/
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoServer
+{
+    class Server
+    {
+        public Server()
+        {
+            
+        }
+
+        public TcpListener Initialize(string ip = "")
+        {
+            IPAddress addr;
+            TcpListener listener;
+
+            // Verifica o endereço de IP
+            if(string.IsNullOrEmpty(ip)) 
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Any, Program.port);
+
+                Console.WriteLine($"Inicializando o cliente com o IP {ep.Address.ToString()}");
+
+                listener = new TcpListener(ep);
+            } else {
+                addr = IPAddress.Parse(ip);
+
+                Console.WriteLine($"Inicializando o cliente com o IP {ip}");
+
+                // Inicia a escuta por conexões
+                listener = new TcpListener(addr, 8085);
+            }
+            
+            listener.Start();
+
+            return listener;
+        }
+
+        public TcpListener Initialize(string ip = "", int port = 8085)
+        {
+            IPAddress addr;
+            TcpListener listener;
+
+            // Verifica o endereço IP
+            addr = IPAddress.Parse(ip);
+
+            // Inicia a escuta por conexões
+            listener = new TcpListener(addr, port);
+            listener.Start();
+
+            return listener;
+        }
+    }
+}
